737eb58dcdc406ec944e4f2d9d2ae858efff8adf
Irdina Batrisyia <68364033+ird1natris@users.noreply.github.com>
2025-06-05 11:16:26 +0800

    Update multi_tier_cloud_app.py
    Fix: corrected ElastiCache import in multi-tier app diagram

--- diagrams/multi_tier_cloud_app.py
+++ diagrams/multi_tier_cloud_app.py
@@ -1,7 +1,7 @@
 from diagrams import Cluster, Diagram
 from diagrams.aws.compute import EC2, Lambda, ECS
 from diagrams.aws.network import ELB, Route53, VPC, PrivateSubnet, PublicSubnet
-from diagrams.aws.database import RDS, ElasticCache, Dynamodb
+from diagrams.aws.database import RDS, ElastiCache, Dynamodb
 from diagrams.aws.integration import SQS, SNS
 from diagrams.aws.security import WAF, Shield, IAM
 from diagrams.onprem.client import Users
